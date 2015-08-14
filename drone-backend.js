a354b55bb5e7f1c7538b64bf8bed33455dc75e89
V3513 (Conference ID) <v3513@cam.ac.uk>
2015-08-14 14:34:58 +0100

    drone1

--- drone-backend.js
+++ drone-backend.js
@@ -1,6 +1,7 @@
 var Cylon = require('cylon');
 var bot;
 
+
 // Initialise the robot
 Cylon.robot()
     .connection("ardrone", {
@@ -11,11 +12,51 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "ardrone-nav",      // Combine with a second device to have more information
+        connection: "ardrone"
+    })
     .on("ready", fly);
-    
+
+var bot;
 // Fly the bot
 function fly(robot) {
+    bot = robot;
+    // Only retrieve a limited amount of navigation data
+    // As recommended by Parrot AR Drone developer's guide
+    bot.drone.config('general:navdata_demo', 'TRUE');
 
+    bot.nav.on("navdata", function(data) {
+        // console.log(data);
+    });
+
+    bot.nav.on("altitudeChange", function(data) {
+        console.log("Altitude:", data);
+        // Drone is higher than 1.5 meters up
+        if (altitude > 1.5) {
+            bot.drone.land();
+        }
+    });
+
+    bot.nav.on("batteryChange", function(data) {
+        console.log("Battery level:", data);
+    });
+
+    // Disable emergency setting if there was any
+    bot.drone.disableEmergency();
+    // Tell the drone it is lying horizontally
+    bot.drone.ftrim();
+
+    // Take off
+    bot.drone.takeoff();
+
+    after(10*1000, function() {
+        bot.drone.land();
+    });
+    after(15*1000, function() {
+        bot.drone.stop();
+    });
 }
+
 
 Cylon.start();
--- drone-frontend.js
+++ drone-frontend.js
@@ -6,15 +6,46 @@ function init() {
 function initTracker(element) {
     // Initialise a color tracker
     var tracker = new tracking.ColorTracker();
-    
+
+    TrackerUtils.addTrackingColor("#A94A45", "red", tracker);
+    TrackerUtils.addTrackingColor("#5EA24E", "green", tracker);
+    TrackerUtils.addTrackingColor("#CB7F84", "magenta", tracker);
     TrackerUtils.startTrackingColors(tracker);
+    
 
     // Whenever there is a new color detected, mark them
     tracker.on('track', function(event) {
-        console.log(event.data);
+        markColors(event.data, element);
     });
 
     return tracker;
+}
+
+function markColors(colors, element) {
+    // Remove previously drawn rectangles indicating detected colors
+    // Get the drawing surface
+    var canvas = $(element + ' .canvas').get(0);
+    var context = canvas.getContext('2d');
+    context.clearRect(0, 0, context.width, context.height);
+
+    for (var i = 0; i < colors.length; i++) {
+        drawRectangle(colors[i], context);
+        writeRectangle(colors[i], element + " .output");
+    }
+}
+
+// Draw an overlay marking the detected color rectangle
+function drawRectangle(rect, context) {
+    context.strokeStyle = rect.color;
+    context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+}
+
+// Write the information out in text
+function writeRectangle(rect, element) {
+    $(element)
+        .append("<p>")
+        .append(rect.color + ": " + rect.width + "X" + rect.height)
+        .append(" @ " + rect.x + ":" + rect.y)
 }
 
 window.addEventListener("load", init);
